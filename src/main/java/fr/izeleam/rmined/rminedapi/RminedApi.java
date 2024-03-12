eaf0b0bffc458ec407f92fb4311b5f9abbb32425
Luca <luca.pourceau@gmail.com>
2024-03-12 14:11:56 +0100

    init:project

---
+++ src/main/java/fr/izeleam/rmined/rminedapi/RminedApi.java
@@ -0,0 +1,17 @@
+package fr.izeleam.rmined.rminedapi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RminedApi extends JavaPlugin {
+
+  @Override
+  public void onEnable() {
+    // Plugin startup logic
+
+  }
+
+  @Override
+  public void onDisable() {
+    // Plugin shutdown logic
+  }
+}
